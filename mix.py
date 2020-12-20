5623cb890484db73efb77def1bf6f9a33c6dc3ec
EasternJournalist <47034050+EasternJournalist@users.noreply.github.com>
2020-12-20 17:37:57 +0800

    Update mix.py

--- mix.py
+++ mix.py
@@ -1,21 +1,28 @@
 import cv2
 import numpy as np
+#from cv2.ximgproc import guidedFilter
 
-src = cv2.imread("fake_0.png")  # 马
-dst = cv2.imread("fake_1.png")  # 背景
-mask = cv2.imread("mask.png")
+def mix(src_path, dst_path, mask_path):
+    " 融合图像 "
+    src = cv2.imread(src_path)  # 马
+    dst = cv2.imread(dst_path)  # 背景
+    mask = cv2.imread(mask_path)
 
-mask = np.clip(np.array(cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)).astype(np.int) * 255, 0, 255).astype(np.uint8)
+    mask = cv2.GaussianBlur(mask, (9, 9), 3)
+    mask = np.clip(np.array(cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)).astype(np.int) * 255, 0, 255).astype(np.uint8)
+    
+    cnts = cv2.findContours(mask, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] 
+    x, y, w, h = cv2.boundingRect(cnts[0])
+    cX = x + w//2
+    cY = y + h//2
+    center = (cX, cY)
 
-cnts = cv2.findContours(mask, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-cnts = cnts[0] 
-x, y, w, h = cv2.boundingRect(cnts[0])
-cX = x + w//2
-cY = y + h//2
-center = (cX, cY)
+    rst = cv2.seamlessClone(src, dst, mask, center, cv2.NORMAL_CLONE)
+    return rst
 
-rst = cv2.seamlessClone(src, dst, mask, center, cv2.NORMAL_CLONE)
 
-cv2.imshow("rst", rst)
-cv2.imwrite("result.png", rst)
-cv2.waitKey(0)
+if __name__ == "__main__":
+    cv2.imshow("rst", mix("fake_0.png", "fake_1.png", "mask.png"))
+    #cv2.imwrite("result.png", rst)
+    cv2.waitKey(0)
