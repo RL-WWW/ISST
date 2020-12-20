79f233eaa91d80bb01cf4b2023daddd419d5b983
wangyu-ustc <1294837640@qq.com>
2020-12-20 13:58:20 +0800

    update code

--- ISST.py
+++ ISST.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import torch
+import numpy as np
 import os
 import torchvision
 from data import create_dataset
@@ -8,17 +9,45 @@ from util import util
 from util.visualizer import save_images
 import torchvision.transforms as tfs
 from options.test_options import TestOptions
+from classes_to_models import get_mapping
 import argparse
 import sys
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--image", default="C:/Users/ls/Desktop/exp/exp.png", help="The image we need to transfer")
-    parser.add_argument("--real_image", default="C:/Users/ls/Desktop/exp/orig_exp.jpg", help="The original image")
+    parser.add_argument("--image", default="C:/Users/ls/Desktop/exp/horse_3.png", help="The image we need to transfer")
+    parser.add_argument("--real_image", default="C:/Users/ls/Desktop/exp/horse_2.jpg", help="The original image")
     parser.add_argument("--temp_folder", default=None, help='Where to put the middle images')
     parser.add_argument("--target_path", default=None, help='Where to put the final result')
     return parser.parse_args()
+
+def config_models_and_datasets(model_names):
+    opt = None
+    models = dict()
+    for name in model_names:
+        sys.argv = sys.argv[:1]
+        sys.argv.append("--dataroot")
+        sys.argv.append(folder)
+        sys.argv.append("--name")
+        sys.argv.append(name)
+        sys.argv.append("--model")
+        sys.argv.append("test")
+        sys.argv.append("--no_dropout")
+        opt = TestOptions().parse()
+        # hard-code some parameters for test
+        opt.num_threads = 0  # test code only supports num_threads = 0
+        opt.batch_size = 1  # test code only supports batch_sized = 1
+        opt.serial_batches = True  # disable data shuffling; comment this line if results on randomly chosen images are needed.
+        opt.no_flip = True  # no flip; comment this line if results on flipped images are needed.
+        opt.display_id = -1  # no visdom display; the test code saves the results to a HTML file.
+
+        model = create_model(opt)
+        model.setup(opt)
+        models[name] = model
+        print(f"model {name} instantiated")
+    dataset = create_dataset(opt)
+    return dataset, models
 
 
 if __name__ == '__main__':
@@ -40,11 +69,13 @@ if __name__ == '__main__':
     if not os.path.exists(folder):
         os.mkdir(folder)
 
-    # real_img = totensor(Image.open("exp/exp_real.png"))
+    classes, models, classes_to_models = get_mapping()
+    # map class to a list of models
 
-    target_path = args.target_path if args.target_path is not None else os.path.dirname(args.image)
+    # image_classes = ["bag", "bed", "car"]
+    image_classes = ['sky', "horse"]  # classes of the processed images
 
-    new_image = torch.zeros_like(img)
+    target_path = args.target_path if args.target_path is not None else os.path.join(os.path.dirname(args.image), "results")
 
     for i, pixel in enumerate(torch.unique(img_2_dim)):
         plane = torch.zeros_like(img)
@@ -53,46 +84,50 @@ if __name__ == '__main__':
         toimage(plane).save(os.path.join(folder, f"exp{i}.png"))
 
     # 调用cycleGAN
-    model = "horse2zebra_pretrained"
-    sys.argv = sys.argv[:1]
-    sys.argv.append("--dataroot")
-    sys.argv.append(folder)
-    sys.argv.append("--name")
-    sys.argv.append(model)
-    sys.argv.append("--model")
-    sys.argv.append("test")
-    sys.argv.append("--no_dropout")
-    opt = TestOptions().parse()
-    # hard-code some parameters for test
-    opt.num_threads = 0  # test code only supports num_threads = 0
-    opt.batch_size = 1  # test code only supports batch_sized = 1
-    opt.serial_batches = True  # disable data shuffling; comment this line if results on randomly chosen images are needed.
-    opt.no_flip = True  # no flip; comment this line if results on flipped images are needed.
-    opt.display_id = -1  # no visdom display; the test code saves the results to a HTML file.
+    model_names = []
+    for _, model_for_class in classes_to_models.items():
+        for model_name in model_for_class:
+            if model_name not in model_names:
+                model_names.append(model_name)
+    print("total model names:")
+    print(model_names)
+    dataset, models = config_models_and_datasets(model_names)
 
-    dataset = create_dataset(opt)
-    model = create_model(opt)
-    model.setup(opt)
-    for i, (pixel, data) in enumerate(zip(torch.unique(img_2_dim), dataset)):
-        model.set_input(data)  # unpack data from data loader
-        model.test()  # run inference
-        visuals = model.get_current_visuals()  # get image results
-        img_path = model.get_image_paths()
+    shape = []
+    num = 1
+    for class_name in image_classes:
+        shape.append(len(classes_to_models[class_name]))
+        num *= shape[-1]
+    indexes = torch.arange(num).reshape(shape)
+    new_images = torch.zeros(num, *(img.shape))
 
-        toimage(visuals['real'].cpu().squeeze()).save("try_real.png")
+    for i, (pixel, data, class_name) in enumerate(zip(torch.unique(img_2_dim), dataset, image_classes)):
+        # get models
+        for j, model_name in enumerate(classes_to_models[class_name]):
+            model = models[model_name]
 
-        # A_path = data['A_paths'][0].split('.')
-        # A_path[0] = A_path[0] + '_fake'
-        # A_path = '.'.join(A_path)
-        # toimage(visuals['fake'].squeeze()).save(A_path)
-        # for i,pixel in enumerate(torch.unique(img_2_dim)):
-        #     fake_im = Image.open(f"exp/exp{i}_fake.png")
-        fake_im = toimage(util.tensor2im(visuals['fake']))
-        fake = totensor(torchvision.transforms.functional.resize(fake_im, (img.shape[1], img.shape[2]), interpolation=2))
-        for i in range(len(new_image)):
-            new_image[i][torch.where(img_2_dim == pixel)] = fake[i][torch.where(img_2_dim == pixel)]
+            model.set_input(data)  # unpack data from data loader
+            model.test()  # run inference
+            visuals = model.get_current_visuals()  # get image results
+            img_path = model.get_image_paths()
+
+            # A_path = data['A_paths'][0].split('.')
+            # A_path[0] = A_path[0] + '_fake'
+            # A_path = '.'.join(A_path)
+            # toimage(visuals['fake'].squeeze()).save(A_path)
+            # for i,pixel in enumerate(torch.unique(img_2_dim)):
+            #     fake_im = Image.open(f"exp/exp{i}_fake.png")
+            fake_im = toimage(util.tensor2im(visuals['fake']))
+            fake = totensor(torchvision.transforms.functional.resize(fake_im, (img.shape[1], img.shape[2]), interpolation=2))
+
+            image_index = indexes.transpose(0, i)[j].flatten()
+
+            for k in range(3):
+                for idx in image_index:
+                    new_images[idx][k][torch.where(img_2_dim == pixel)] = fake[k][torch.where(img_2_dim == pixel)]
 
     #     new_image[torch.where(real_img == pixel)] = fake[torch.where(real_img == pixel)]
     #     print(torch.where(real_img == pixel))
-    result = toimage(new_image)
-    result.save(os.path.join(target_path, "result.png"))
+    for i, new_image in enumerate(new_images):
+        result = toimage(new_image)
+        result.save(os.path.join(target_path, f"result_{i}.png"))
